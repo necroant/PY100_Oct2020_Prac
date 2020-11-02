eaae470f430c23091459638251c8431a8f792ee8
PYTHON <1@1.com>
2020-11-02 21:26:33 +0300

    выглядит прикольно но не работает

--- game/game.py
+++ game/game.py
@@ -1,10 +1,11 @@
 import numpy as np
+from random import choice
 
 
 N = 3       # field size
 
-FIRST_PLAYER = 'X'
-SECOND_PLAYER = '0'
+FIRST_PLAYER = 'x'
+SECOND_PLAYER = 'o'
 EMPTY_CELL = '.'
 
 INIT_FIELD_SET = {
@@ -25,19 +26,48 @@ INIT_FIELD_SET = {
     ),
 }
 
+
 def get_first_step():
-    ...
+    if choice([FIRST_PLAYER, SECOND_PLAYER]) == FIRST_PLAYER:
+        return FIRST_PLAYER, SECOND_PLAYER
+    else:
+        return SECOND_PLAYER, FIRST_PLAYER
+
 
 
 def step(char, field):
-    ...
+    while True:
+        cell = input(f"<{char}> Введите координату ячейки: ")
+        cell.upper()
+        if cell in field and cell != FIRST_PLAYER and cell != SECOND_PLAYER:
+            row, col = find_index_cell(cell, field)
+            field[row, col] = char
+            break
+        print("Введите корректную ячейку!")
+
+    return field
 
 
-def enemy_step(char):
-    step(char)
+
+
+def find_index_cell(char, field):
+    """
+    :param char: Character in the field
+    :param field: Field in which to look
+    :return: x, y coordinates on the field
+    """
+    for i in range(N):
+        for j in range(N):
+            if field[i, j] == char:
+                return i, j
+
+
+def enemy_step(char, field):
+    step(char, field)
 
 
 def display_field(field):
+    print()
     print(u'┌', end='')
     for i in range(N):
         print(u'─', end='')
@@ -74,13 +104,61 @@ def init_field():
     return field
 
 
-def check_win():
-    ...
+def check_sequence(list_, char):
+    for value in list_:
+        if value != char:
+            return False
+
+    return True
+
+def check_win(field, char):
+    victory = False
+    for row in field:                           # Проверяем строки
+        if check_sequence(row, char):
+            victory = True
+
+    if check_sequence(field.diagonal(), char):  # Проверяем главную диагональ
+        victory = True
+
+    r90 = np.rot90(field)
+    for col in r90:                             # Проверяем столбцы
+        if check_sequence(col, char):
+            victory = True
+
+    if check_sequence(r90.diagonal(), char):    # Проверяем побочную диагональ
+        victory = True
+
+    return victory
+
+
+
+
+def display_victory_screen(field):
+    for i in range(N):
+        for j in range(N):
+            if field[i,j] in INIT_FIELD_SET[N]:
+                field[i,j] = EMPTY_CELL
+
+    display_field(field)
 
 
 def main():
     field = init_field()
     display_field(field)
+    current_player, other_player = get_first_step()
+    player_dict = {
+        current_player: step,
+        other_player: enemy_step
+    }
+    while True:
+        field = player_dict[current_player](current_player, field)
+        if check_win(field, current_player):
+            print(f"Игрок <{current_player}> выиграл")
+            break
+        display_field(field)
+        current_player, other_player = other_player, current_player
+
+    display_victory_screen(field)
 
 
 if __name__ == '__main__':
